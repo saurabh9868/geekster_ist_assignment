19b1dd75b1239dfdaf0bb52155ccc07c7a087708
saurabh <sgarg8678@gmail.com>
2021-12-14 14:18:46 +0530

    ist question

---
+++ ist_assignment.java
@@ -0,0 +1,10 @@
+package saurabh_geekster;
+
+import java.util.Scanner;
+
+public class ist_assignment {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        
+    }
+}
